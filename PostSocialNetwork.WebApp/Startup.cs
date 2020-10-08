ca24caad24227812375a664fb749f1ec7b4a5307
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-10-07 22:24:31 -0300

    Pressed ENTER.

--- PostSocialNetwork.WebApp/Startup.cs
+++ PostSocialNetwork.WebApp/Startup.cs
@@ -47,6 +47,7 @@ namespace PostSocialNetwork.WebApp
             }
             else
             {
+
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
